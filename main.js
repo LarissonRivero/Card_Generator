9af5ccc31bd7147dea09036b2ec1eb5172fbddc7
LarissonRivero <Larissonrivero2@gmail.com>
2023-08-23 20:21:22 -0400

    Logica del palo de la parte de Inferior, Copiando el superior

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
+/**@param {string} palos; array de palos
+ * @param {string and number} valores; array de valores
+ * @function generadorElementos toma como argumento un array y devuelve en un nuevo array de elementos aleatorios
+ * @function generadorCartas genera una combinacion aleatoria y la muestra en el html- Esta el la función principal la cual se asocia al evento click del botón
+ * @constant paloTopHtml es el elemento del html donde se muestrara el palo de la carta
+ * @const copyPalo es una copia del palo de la carta que se muestrara en la parte inferior derecha de la carta
+ * @operadores ternarios; se utilizan para cambiar el color de los palos de negro a rojo y viceversa, reemplazando el if y else.
+ */
+
 const palos = ["♠", "♦", "♥", "♣"];
 const valores = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-function generadorElementos(arr) { // Función que genera elementos de un array y los devuelve en un nuevo array
-  const randomIndex = Math.floor(Math.random() * arr.length);// Genera un número aleatorio entre 0 y la longitud del array
+function generadorElementos(arr) { 
+  const randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
 }
 
-function generadorCartas() { // Función que genera una carta aleatoria
+function generadorCartas() {
     const palo = generadorElementos(palos);
     const valor = generadorElementos(valores);
-    const paloHtml = document.getElementById("iconTop"); 
-    const valorHtml = document.getElementById("num");
 
-    paloHtml.innerHTML = `${palo}`; 
+    const paloTopHtml = document.getElementById("iconTop"); 
+    const valorHtml = document.getElementById("valor");
+
+    paloTopHtml.innerHTML = `${palo}`; 
     valorHtml.innerHTML = `${valor}`;
+    
+    const copyPalo = paloTopHtml.textContent;
+    iconBot.textContent = copyPalo;
 
-    if (palo === "♦" || palo === "♥") {
-        paloHtml.style.color = "red";
-        valorHtml.style.color = "red";
-    } 
-    else {
-        paloHtml.style.color = "black";
-        valorHtml.style.color = "black";
-    }
+    paloTopHtml.style.color = (palo === "♦" || palo === "♥") ? "red" : "black";
+    valorHtml.style.color = (palo === "♦" || palo === "♥") ? "red" : "black";
+    iconBot.style.color = (palo === "♦" || palo === "♥") ? "red" : "black";
 
-    console.log(palo,valor)
+    console.log(palo,valor,palo)
 }
 
 
